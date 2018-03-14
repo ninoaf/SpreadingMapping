217e09edf66e9def58a5dfe4f687622a1a37e872
Nino Antulov-Fantulin <nino.naf@gmail.com>
2018-03-14 17:29:43 +0100

    Update SDPN_sparse.m

--- SDPN_sparse.m
+++ SDPN_sparse.m
@@ -1,7 +1,7 @@
 function [ D ] = SDPN_sparse( A, N, E, beta, gamma,  source )
 % INPUT: adjacency matrix A (unweighted), number of nodes N, number of edges E,
 % parameters of SIR beta,gamma, id of source node
-% OUTPUT: Vector of first infection from source to other nodes
+% OUTPUT: Vector of first infection times from source to other nodes
 
 Rho = exprnd(1/beta, E, 1);
 
