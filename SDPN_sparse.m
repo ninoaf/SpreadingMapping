e145623f507c4736df08e472633b41c75dcd4295
Nino Antulov-Fantulin <nino.naf@gmail.com>
2018-03-14 17:22:42 +0100

    Add files via upload

---
+++ SDPN_sparse.m
@@ -0,0 +1,16 @@
+function [ D ] = SDPN_sparse( A, N, E, beta, gamma,  source )
+% INPUT: adjacency matrix A, number of nodes N, number of edges E,
+% parameters of SIR beta,gamma, id of source node
+% OUTPUT: Vector of first infection from source to other nodes
+
+Rho = exprnd(1/beta, E, 1);
+
+Tau_node = exprnd(1/gamma,N,1);
+[I,J]=find(A);
+Tau = Tau_node(I);
+
+W = sparse(I,J,Rho.*(Rho <= Tau),N,N);
+D = graphshortestpath(W, source);
+
+end
+
